5d84bdc85699fde833a4115b656e8a108a0b268d
jingtao8a <1921056015@qq.com>
2024-09-25 19:20:00 -0700

    add microBenchmark.sh

---
+++ microBenchmark.sh
@@ -0,0 +1,9 @@
+./build/microbench \
+--keys_file=/home/jingtao8a/GRE/datasets/books_200M_uint32 \
+--keys_file_type=binary \
+--read=0.5 --insert=0.5 \
+--operations_num=800000000 \
+--table_size=-1 \
+--init_table_ratio=0.5 \
+--thread_num=1 \
+--index=DILI
